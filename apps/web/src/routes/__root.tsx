cb3586444a510ca73029252c2a9e5953c86a3cfa
khalludi03 <khaledjb03121@gmail.com>
2026-02-01 20:36:58 +0600

    chore: remove unnecessary things

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -51,6 +51,15 @@ function RootDocument() {
     <html lang="en">
       <head>
         <HeadContent />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.addEventListener('load', () => {
+                document.documentElement.classList.add('theme-transition-enabled');
+              });
+            `,
+          }}
+        />
       </head>
       <body className="min-h-screen flex flex-col">
         <ToastProvider>
