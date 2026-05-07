802fb64a5e8c68ca496b963738962226bcd7f6e7
khalludi03 <khaledjb03121@gmail.com>
2026-05-07 18:13:57 +0600

    fix: drop and restore default around rate_limit count type cast

--- packages/db/src/migrations/0014_breezy_starfox.sql
+++ packages/db/src/migrations/0014_breezy_starfox.sql
@@ -1,1 +1,3 @@
+ALTER TABLE "rate_limit" ALTER COLUMN "count" DROP DEFAULT;
 ALTER TABLE "rate_limit" ALTER COLUMN "count" SET DATA TYPE integer USING count::integer;
+ALTER TABLE "rate_limit" ALTER COLUMN "count" SET DEFAULT 0;
